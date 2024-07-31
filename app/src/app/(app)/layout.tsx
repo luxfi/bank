2508039afe87bd33b20ea4befb44a35c276918bd
haoliu716 <alan193716@gmail.com>
2024-07-31 22:34:21 +0300

    fix runtime issue

--- app/src/app/(app)/layout.tsx
+++ app/src/app/(app)/layout.tsx
@@ -32,12 +32,31 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   }, [currentUser]);
 
   return (
-    <Layout>
-      <Header />
-      <Navbar />
-      <Main>
-        <ContentContainer>{children}</ContentContainer>
-      </Main>
-    </Layout>
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/image/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#00569e" />
+        <meta name="description" content="CDAX Forex Control Panel" />
+        <meta property="og:title" content="CDAX Forex" />
+        <meta property="og:description" content="CDAX Forex Control Panel" />
+        <meta
+          property="og:image"
+          content="https://app.cdaxforex.com/images/snapshot.png"
+        />
+        <meta property="og:url" content="app.cdaxforex.com" />
+        <meta property="og:type" content="website" />
+        <title>CDAX Forex</title>
+      </head>
+      <body>
+        <Layout>
+          <Header />
+          <Navbar />
+          <Main>
+            <ContentContainer>{children}</ContentContainer>
+          </Main>
+        </Layout>
+      </body>
+    </html>
   );
 }
