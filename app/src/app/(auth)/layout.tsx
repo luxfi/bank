65e9c63d44b7b1956c488aed6aa96c19a0e59aff
haoliu716 <alan193716@gmail.com>
2024-07-31 23:36:46 +0300

    update next version from 14.1.4 to 14.2.4

--- app/src/app/(auth)/layout.tsx
+++ app/src/app/(auth)/layout.tsx
@@ -4,25 +4,8 @@ export default function BaseLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/image/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#00569e" />
-        <meta name="description" content="CDAX Forex Control Panel" />
-        <meta property="og:title" content="CDAX Forex" />
-        <meta property="og:description" content="CDAX Forex Control Panel" />
-        <meta
-          property="og:image"
-          content="https://app.cdaxforex.com/images/snapshot.png"
-        />
-        <meta property="og:url" content="app.cdaxforex.com" />
-        <meta property="og:type" content="website" />
-        <title>CDAX Forex</title>
-      </head>
-      <body>
-        {children}
-      </body>
-    </html>
+    <>
+      {children}
+    </>
   );
 }
