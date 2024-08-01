7e8deb90c6591c991476755ccd2205b0e2e37410
Zach Kelling <z@zeekay.io>
2024-08-01 02:41:52 -0700

    Fix beneficary emails

--- api/src/users/emails/beneficiary-email.ts
+++ api/src/users/emails/beneficiary-email.ts
@@ -10,9 +10,9 @@ interface BeneficiaryEmailContext {
     message: string;
 }
 
-const heroImage = readFileSync(join(__dirname, 'templates/emails/operations/beneficiary/hero.gif'),).toString('base64');
-const helpCenterImage = readFileSync(join(__dirname, 'templates/emails/operations/beneficiary/help-centre-btn.gif')).toString('base64');
-const icon1Image = readFileSync(join(__dirname, 'templates/emails/operations/beneficiary/icon-1.gif')).toString('base64');
+const heroImage = readFileSync(join(__dirname, '../../templates/emails/operations/beneficiary/hero.gif'),).toString('base64');
+const helpCenterImage = readFileSync(join(__dirname, '../../templates/emails/operations/beneficiary/help-centre-btn.gif')).toString('base64');
+const icon1Image = readFileSync(join(__dirname, '../../templates/emails/operations/beneficiary/icon-1.gif')).toString('base64');
 
 export default class BeneficiaryEmail extends BaseEmail<BeneficiaryEmailContext> {
     constructor(to: string, context: BeneficiaryEmailContext) {
