b96d6ba32614e627b4c62a27fc18c4586871cb7f
Zach Kelling <z@zeekay.io>
2026-03-17 20:44:57 -0700

    fix: use ARC runners for CI builds, fix TradingModule DI for ClientsRepository

--- app/api/src/trading/trading.module.ts
+++ app/api/src/trading/trading.module.ts
@@ -1,14 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { RegisteredForClients } from '@luxbank/tools-models';
+import { MikroOrmRegisteredForClients, MikroOrmRegisteredForUserClients } from '@luxbank/tools-models';
 import { IamModule } from '@luxbank/iam';
+import { ClientsModule } from '../clients/clients.module';
 import { ComplianceSyncService } from './compliance-sync.service';
 import { TradingService } from './trading.service';
 
 @Module({
   imports: [
     ConfigModule,
-    RegisteredForClients(),
+    MikroOrmRegisteredForClients(),
+    MikroOrmRegisteredForUserClients(),
+    forwardRef(() => ClientsModule),
     IamModule.forRoot(),
   ],
   providers: [ComplianceSyncService, TradingService],
